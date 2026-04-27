107ab984a085515d85cb8d4070025fd68f211278
James <jamesoffice.417@gmail.com>
2026-04-27 18:10:00 +0800

    init

---
+++ app/pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const { data: home } = await useAsyncData(() => queryCollection('content').path('/').first())
+
+useSeoMeta({
+  title: home.value?.title,
+  description: home.value?.description,
+})
+</script>
+
+<template>
+  <div>
+    <UButton>Button</UButton>
+
+    <ContentRenderer
+      v-if="home"
+      :value="home"
+    />
+    <div v-else>
+      Home not found
+    </div>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/content'],
+
+  devtools: { enabled: true },
+
+  css: ['~/assets/css/main.css'],
+
+  compatibilityDate: '2025-07-15',
+
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+})
