e06b62ed454ccf9019d3cdc518820217889e2a4f
James <jamesoffice.417@gmail.com>
2026-04-27 22:44:13 +0800

    dep: add @vueuse/nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/content'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/content', '@vueuse/nuxt'],
 
   devtools: { enabled: true },
 
