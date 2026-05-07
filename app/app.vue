9e299b1b732ad6ff223c0f9eda0aedb4849e3205
James <jamesoffice.417@gmail.com>
2026-05-07 21:24:39 +0800

    style: font and theme color

--- app/app.vue
+++ app/app.vue
@@ -4,6 +4,15 @@ import { en, zh_tw } from '@nuxt/ui/locale'
 const { locale } = useI18n()
 
 const appLocale = computed(() => locale.value === 'zh' ? zh_tw : en)
+const lang = computed(() => appLocale.value.code)
+const dir = computed(() => appLocale.value.dir)
+
+useHead({
+  htmlAttrs: {
+    lang,
+    dir,
+  },
+})
 </script>
 
 <template>
